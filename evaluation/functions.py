4086a57ec89f2a866d47f779746a60accc5683f0
albertonuzzaci <alberto.nuzzaci@gmail.com>
2024-05-15 15:37:37 +0200

    Fixed benchmarks

--- evaluation/functions.py
+++ evaluation/functions.py
@@ -7,36 +7,10 @@ from functools import reduce
 from index import Index
 from model import IRModel
 #Import the models
-from whoosh.scoring import BM25F
-from doc2vec.doc2vec_model import Doc2VecModel
-from sentiment.sentiment_model import SentimentWeightingModel, AdvancedSentimentWeightingModel
 
-models = [
-	BM25F() 
- 	#Doc2VecModel(),
-	#SentimentWeightingModel(),
-	#AdvancedSentimentWeightingModel()
-]
 
-query_test = {
-	"UIN" : "enjoyable double room",
-	"query" : "double room",
-	"sentiments" : [
-        "joy", "surprise"
-    ],
-	"relevant_documents" : [
-        8032080, 13763334, 19636343
-    ]
-}
 
-query_test_2 = {
-        "uin": "I need an apartment near st james park",
-        "query": "apartment near st james",
-        "sentiments":[],
-        "relevant_documents": [
-            44652702,17989962,6430495,561569908310899814,541903175815998607,15336396,15954244,51592505,544034283385258902,584579738630287719,49478531,51592515,51677622
-        ]
-    }
+
 
 class Benchmark():
     
@@ -50,7 +24,7 @@ class Benchmark():
         results = [int(id) for id in result.keys()]
         if verbose:
             print(f'Results: {results}\nRelevant documents: {self.query["relevant_documents"]}')
-        print(f'Relevant retrived: {set(results).intersection(set(self.query["relevant_documents"]))}')
+            print(f'Relevant retrived: {set(results).intersection(set(self.query["relevant_documents"]))}')
         return results
     
     def recall(self, R, A):
@@ -79,110 +53,66 @@ class Benchmark():
             print(f'Recall values: {recallValues}')
         
         return recallValues
-    
-    def getNRLValues(self, model, nResult, verbose=False):
-        results = self.getResults(nResult, model, verbose)
-        p_val = self.getPrecisionValues(results, verbose)
-        r_val = self.getRecallValues(results, verbose)
-        NPRvalues = list(zip(r_val, p_val))
-        if verbose:
-            print(f'Recall-Precision Values: {NPRvalues}')
-        
-        return NPRvalues
-    
-    def getSRLValues(self, pNRL):
-        i = 0
-        pSRL = [(0, pNRL[i][1])]
-        for r in range(1, 11):
-            if (i+1 < len(pNRL) and r/10 > pNRL[i][0]):
-                i = i + 1
-            last = i - 1 if i > 0 else 0
-            next = i
-            pSRL.append((r/10, max(pNRL[last][1], pNRL[next][1])))
-        return pSRL
-    import numpy as np
-    
-    def getSRLValuesChatty(self, precision, recall):
+   
+   
+    def getSRLValues(self, precision, recall, verbose = False):
         levels = [i / 10 for i in range(11)] # Livelli di richiamo standard: 0.0, 0.1, ..., 1.0
         srlValues = []
+        
+        NRLvalues = zip(precision, recall)
+        
+        if verbose:
+            print(f'Natural Recall-Precision Values {list(NRLvalues)}')
+
         for level in levels:
-            precisions = [p for p, r in zip(precision, recall) if r >= level]
+            precisions = [p for p, r in NRLvalues if r >= level]
             if precisions:
                 srlValues.append(max(precisions))
             else:
                 srlValues.append(0.0)
 
-        return list(zip(levels, srlValues))
+        srlValues = list(zip(levels, srlValues))
+        
+        if verbose:
+            print(f'Standard Recall-Precision Values {srlValues}')
+            
+        return srlValues
     
-    
-    def plot(self, prVal):
-                
-        x, y = zip(*prVal)
-        
-        plt.figure(figsize=(8, 6))
-        plt.plot(x, y, 'bo')  
-        
-        plt.xlim(0, 1)
-        plt.ylim(0, 1)
-        
-        plt.xlabel('Recall')
-        plt.ylabel('Precision')
-        plt.title(f'Query: {self.query["query"]}')
-
-    def plot_sns():
-        # plot the precisions
-        # apply the default theme
-        sns.set_theme()
-
-        axes = ["standard recall levels", "precision"]
-
-        dfB = pd.DataFrame(pB, columns = axes)
-        dfB["Version"] = "Base"
-
-        dfAV = pd.DataFrame(pAV, columns = axes)
-        dfAV["Version"] = "Sentiment Analysis AV"
-
-        dfINAV = pd.DataFrame(pINAV, columns = axes)
-        dfINAV["Version"] = "Sentiment Analysis INAV"
-
-        # create a dataframe for Seaborn
-        df = pd.concat([dfB, dfAV, dfINAV])
-
-        # print the examined query
-        b.print_query(examined_q)
-
-        # plot the line graph
-        pltP = sns.lineplot(data = df, x = 'standard recall levels', y = 'precision', marker='o', markersize=8, 
-                    hue="Version", palette="colorblind")
-
-        # set fixed axes, the semicolon suppress the output
-        pltP.set_xlim([-0.05, 1.05])
-        pltP.set_ylim([-0.05, 1.05])
 
 if __name__ == "__main__":
+    
     bench_test = Benchmark(query_test_2)
     
-    for i in models:
-        print("\n",i.__class__.__name__)
-        results = bench_test.getResults(10, i)
-        NRLVal = bench_test.getNRLValues(i, 10, True)
-        SRLVal = bench_test.getSRLValues(NRLVal)
-        chatty = bench_test.getSRLValuesChatty(bench_test.getPrecisionValues(results), 
-                                               bench_test.getRecallValues(results))
-        print(SRLVal)
-        bench_test.plot(chatty)
-    plt.show()
-        
-        
-# [(0.08, 1.0), (0.15, 1.0), (0.15, 0.67), (0.23, 0.75), (0.23, 0.6), (0.23, 0.5), (0.23, 0.43), 
-# (0.23, 0.38), (0.23, 0.33), (0.31, 0.4)]
-    # 0.0 -> >= 0.0 = 1.0
-    # 0.1 -> >= 0.1 = 1.0
-    # 0.2 -> >= 0.2 = 0.75
-    # 0.3 -> >= 0.3 = 0.4
-    # 0.4 -> >= 0.4 = 0 
-        
+    axes = ["standard recall levels", "precision"]
+    df = pd.DataFrame()
     
+    for model, model_name in models:
+        result = bench_test.getResults(10, model)
+        SRLValues = bench_test.getSRLValues(
+            bench_test.getPrecisionValues(result),
+            bench_test.getRecallValues(result)
+        )
+        
+        dfB = pd.DataFrame(SRLValues, columns = axes)
+        dfB["Version"] = f'{model_name}'
+        
+        df = pd.concat([df, dfB])
+
+    sns.set_theme()
+
+    # create a dataframe for Seaborn
+
+    # plot the line graph
+    pltP = sns.lineplot(data = df, x = 'standard recall levels', y = 'precision', marker='o', markersize=8, 
+                hue="Version", palette="colorblind")
+
+    # set fixed axes, the semicolon suppress the output
+    pltP.set_xlim([-0.05, 1.05])
+    pltP.set_ylim([-0.05, 1.05])
+    
+    
+
+   
 
 
 		
